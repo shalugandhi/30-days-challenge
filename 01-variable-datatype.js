953887e741110870342996e3993b0a98128453d3
shalugandhi <shalakagandhi81@gmail.com>
2024-07-14 19:58:37 +0530

    variable js

---
+++ 01-variable-datatype.js
@@ -0,0 +1,57 @@
+// Activity 1: Variable Declaration
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+var num = 12;
+console.log(num);
+
+// Task 1: Declare a variable using let, assign it a string, and log the value to the console.
+
+let str = 'javascript with chai aur code';
+console.log(str);
+
+// Activity 2: Constant Declaration
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const isLogin = true;
+console.log(isLogin);
+
+// Activity 4: DataTypes
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+// Number
+var numVar = 32;
+console.log(typeof numVar);
+
+// String
+let strVar = "I'm learning javascript with chai aur code by Hitesh sir";
+console.log(typeof strVar);
+
+//Bollean
+let boolVar = true;
+console.log(typeof boolVar);
+
+//Object
+const personalInfo = {
+        name: 'Shalaka',
+        age: 28,
+        JobTitle: 'Front-End Developer'
+}
+console.log(typeof personalInfo);
+
+//array
+const skills = ['HTML', 'CSS', 'Javascript', 'ReactJS'];
+console.log(typeof skills);
+
+// Activity 4: Reassigning Variables
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+let a = 0;
+console.log("Initial Value", a);
+
+a = 10;
+console.log("New Value", a);
+
+// Activity 5: Understanding const
+// Task 6: Try reassigning a variable declared with const and observe the error.
+const ecommercePlatform = 'Amazon';
+ecommercePlatform = 'Flipkart';
+console.log(ecommercePlatform);
+
+
